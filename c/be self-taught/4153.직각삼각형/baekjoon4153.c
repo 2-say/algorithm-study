a92a9c92fcad5e31581928e5742ccc6a6c24418c
sehee <tpgml701@naver.com>
2023-12-13 12:49:18 +0900

    feat: BOJ 4153

--- c/be self-taught/4153.직각삼각형/baekjoon4153.c
+++ c/be self-taught/4153.직각삼각형/baekjoon4153.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
 #include <math.h>
+#include <limits.h>
+#include <stdlib.h>
 
-int main() {
-    double a,b,c;
-    scanf("%lf", &a);
-    scanf("%lf", &b);
-    scanf("%lf", &c);
-    
-    if(exp2(a) + exp2(b) == exp2(c)) printf("right");
-    else{
-        printf("wrong");
-    }
+int compare(const void *a, const void *b)
+{
+    int num1 = *(int *)a;    
+    int num2 = *(int *)b;   
+    if (num1 < num2)    
+        return -1;     
+    if (num1 > num2)   
+        return 1;     
+    return 0;   
 }
+
+int main()
+{
+    while (1)
+    {   
+        long arr[3];
+        scanf("%ld %ld %ld", &arr[0], &arr[1], &arr[2]);
+        qsort(arr,sizeof(arr)/ sizeof(long), sizeof(long),compare);
+        if(arr[0] == 0 && arr[1] ==0 && arr[2]==0) break;
+        if (pow(arr[0],2) + pow(arr[1],2) == pow(arr[2],2))
+            printf("right\n");
+        else{
+            printf("wrong\n");
+        }
+    }
+    return 0;
+}
+
+
