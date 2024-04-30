384e746ee2b9d72d00b8582b0325c7a96195c1bb
Sehee <91319157+2-say@users.noreply.github.com>
2024-04-30 15:41:40 +0900

    [Silver I] Title: IOIOI, Time: 268 ms, Memory: 20052 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -6,38 +6,26 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         int n = Integer.parseInt(br.readLine());
-        int s = Integer.parseInt(br.readLine());
-        int answer = 0;
-        String[] input = br.readLine().split("");
+        int m = Integer.parseInt(br.readLine());
+
+        String str = br.readLine();
+        int cnt = 0, ans = 0;
         
-        StringBuilder P = new StringBuilder();
-        P.append("I");
-        for(int i = 0; i < n ; i++) {
-            P.append("OI");
-        }
-
-        int windowSize = P.length();
-
-
-        StringBuilder tmp = new StringBuilder();
-        
-        for(int i = 0 ; i < windowSize; i++) {
-            tmp.append(input[i]);
-        }
-
-        if(tmp.toString().equals(P.toString())) {
-            answer++;
-        }
-        
-        for(int i = windowSize; i < input.length; i++) {
-            tmp.delete(0,1);
-            tmp.append(input[i]);
-
-            if(tmp.toString().equals(P.toString())) {
-                answer++;
+        for(int i=1; i<m-1; ) {
+            if(str.charAt(i) == 'O' && str.charAt(i+1) == 'I') {
+                cnt++;
+                if(cnt == n) {
+                    if(str.charAt(i-(cnt*2-1)) == 'I')
+                        ans++;
+                    cnt--;
+                }
+                i += 2;
+            }
+            else {
+                cnt = 0;
+                i++;
             }
         }
-        
-        System.out.println(answer);
+        System.out.println(ans);
     }
 }
