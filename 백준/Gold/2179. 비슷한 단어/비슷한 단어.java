48e586c988f415ebd0818915834501177829d087
Sehee <91319157+2-say@users.noreply.github.com>
2024-11-12 23:32:33 +0900

    [Gold IV] Title: 비슷한 단어, Time: 1208 ms, Memory: 18216 KB -BaekjoonHub

--- 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
@@ -1,49 +1,45 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-	
-	static int check(String s1, String s2) {
-		int cnt = 0, size = Math.min(s1.length(), s2.length());
-		for(int i=0;i<size;i++) {
-			if(s1.charAt(i)==s2.charAt(i)) cnt++;
-			else break;
-		}
-		return cnt;
-	}
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		
-		String[] list = new String[N];
-		for(int i=0;i<N;i++) {
-			list[i] = br.readLine();
-		}
-		
-		int ans1=-1,ans2=-1;
-		int max=Integer.MIN_VALUE;
-		
-		for(int i=0;i<N-1;i++) {
-			String s1 = list[i];
-			
-			for(int j=i+1;j<N;j++) {
-				int cnt=0;
-				String s2 = list[j];
 
-				cnt = check(s1,s2);
-				
-				if(cnt>max) {
-					ans1 = i;
-					ans2 = j;
-					max = cnt;
-				}
-			}
-		}
-		
-		System.out.println(list[ans1]);
-		System.out.println(list[ans2]);
-	}
+class Main { 
+    static int N;
+    static String[] arr;
+    static int ai; 
+    static int bi;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new String[N];
+        int minLen = 0;
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = br.readLine();
+        }
+
+        for(int i = 0; i < N-1; i++) {
+            String a = arr[i];            
+            for(int j = i + 1; j < N; j++) {
+                String b = arr[j];
+
+                int aIdx = 0;
+
+                while(aIdx < a.length() && aIdx < b.length()) {
+                    if(a.charAt(aIdx) != b.charAt(aIdx)) break;
+                    aIdx++;
+                }
+
+                if(minLen < aIdx) {
+                    minLen = aIdx; 
+                    ai = i;
+                    bi = j;
+                }
+            }
+        }
+
+        System.out.println(arr[ai]);
+        System.out.println(arr[bi]);
+    }
+
 }
