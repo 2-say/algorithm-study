421b9097919ffa42d28ae5fde0808c408ded1a1b
Sehee <91319157+2-say@users.noreply.github.com>
2024-11-14 21:45:30 +0900

    [Gold IV] Title: 작업, Time: 716 ms, Memory: 96484 KB -BaekjoonHub

--- 백준/Gold/2056. 작업/작업.java
+++ 백준/Gold/2056. 작업/작업.java
@@ -1,85 +1,78 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main {
+class Main {
+    static boolean[] visited;
+    static int[] edgeCount;
+    static int N;
+    static List<Node>[] nList;
+    static int[] time, resultTime;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
+    static class Node implements Comparable<Node> {
+        int e, cost;
 
-		int N = Integer.parseInt(br.readLine());
-		ArrayList<ArrayList<Integer>> a = new ArrayList<>();
+        Node(int e, int cost) {
+            this.e = e;
+            this.cost = cost;
+        }
 
-		for (int i = 0; i <= N; i++) {
-			a.add(new ArrayList<>());
-		}
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(cost, o.cost);
+        }
+    }
 
-		int[] indegree = new int[N + 1];
-		int[] time = new int[N + 1];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
 
-		for (int i = 1; i <= N; i++) {
-			st = new StringTokenizer(br.readLine());
+        nList = new List[N];
+        time = new int[N];
+        resultTime = new int[N];
+        edgeCount = new int[N];
 
-			time[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) nList[i] = new ArrayList<>();
 
-			int num = Integer.parseInt(st.nextToken());
-			for (int j = 0; j < num; j++) {
-				int temp = Integer.parseInt(st.nextToken());
-				a.get(temp).add(i);
+        for (int i = 0; i < N; i++) {
+            int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            time[i] = arr[0];
+            resultTime[i] = time[i];  
+            int jobN = arr[1];
 
-				indegree[i]++;
-			}
-		}
+            for (int j = 0; j < jobN; j++) {
+                int dependency = arr[2 + j] - 1;
+                nList[dependency].add(new Node(i, time[i]));
+                edgeCount[i]++;
+            }
+        }
 
-		bw.write(topologicalSort(N, a, indegree, time) + "\n");
-		bw.flush();
-		bw.close();
-		br.close();
-	}
+        System.out.println(topoSort());
+    }
 
-	// 위상 정렬
-	public static int topologicalSort(int N, ArrayList<ArrayList<Integer>> a, int[] indegree, int[] time) {
-		Queue<Integer> q = new LinkedList<>();
+    static int topoSort() {
+        Queue<Integer> q = new LinkedList<>();
+        int maxTime = 0;
 
-		int[] result = new int[N + 1]; // 각각의 작업을 수행하는 데 걸리는 시간
-		for (int i = 1; i <= N; i++) {
-			result[i] = time[i];
-			
-			// indegree가 0인 작업을 큐에 넣음.
-			if (indegree[i] == 0) {
-				q.offer(i);
-			}
-		}
+        for (int i = 0; i < N; i++) {
+            if (edgeCount[i] == 0) {
+                q.add(i);
+            }
+        }
 
-		while (!q.isEmpty()) {
-			int now = q.poll();
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            maxTime = Math.max(maxTime, resultTime[cur]);
 
-			for (int next : a.get(now)) {
-				indegree[next]--;
+            for (Node next : nList[cur]) {
+                resultTime[next.e] = Math.max(resultTime[next.e], resultTime[cur] + time[next.e]);
+                edgeCount[next.e]--;
 
-				result[next] = Math.max(result[next], result[now] + time[next]);
-				
-				// 새롭게 indegree가 0이 된 작업을 큐에 넣음.
-				if (indegree[next] == 0) {
-					q.offer(next);
-				}
-			}
-		}
+                if (edgeCount[next.e] == 0) {
+                    q.add(next.e);
+                }
+            }
+        }
 
-		int ans = 0;
-		for (int i = 1; i <= N; i++) {
-			ans = Math.max(ans, result[i]);
-		}
-
-		return ans;
-	}
-
+        return maxTime;
+    }
 }
