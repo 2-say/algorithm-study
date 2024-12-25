7cbe47bcce348c9eb6118a9e95e62470bbb343c2
Sehee <91319157+2-say@users.noreply.github.com>
2024-12-25 17:03:44 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,30 +1,24 @@
 import java.util.*;
 
 class Solution {
-    Map<String, Integer> word = new HashMap<>();
-    Map<Integer, Integer> whoSay = new HashMap<>();
-    
     public int[] solution(int n, String[] words) {
-        int[] answer = new int[2];
-        whoSay.put(0, 1);
-        word.put(words[0], 1);
-        
-        for(int i = 1; i < words.length; i++) {
-            int idx = i % n;
-            char[] cur = words[i].toCharArray();
-            char lastC = cur[0];
-            char[] prev = words[i-1].toCharArray();
-            char lastP = prev[prev.length-1];
-            
-            if(word.containsKey(words[i]) || (lastP != lastC)) {
-                answer[0] = idx + 1;
-                answer[1] = whoSay.getOrDefault(idx, 0) + 1;
-                break;
-            } else {
-                word.put(words[i], 1);
-                whoSay.put(idx, whoSay.getOrDefault(idx, 0) + 1);
+        Map<String, Boolean> wordUsed = new HashMap<>();
+        int[] counts = new int[n];
+
+        wordUsed.put(words[0], true);
+        counts[0]++;
+
+        for (int i = 1; i < words.length; i++) {
+            int player = i % n;
+            counts[player]++;
+
+            if (wordUsed.containsKey(words[i]) || words[i - 1].charAt(words[i - 1].length() - 1) != words[i].charAt(0)) {
+                return new int[]{player + 1, counts[player]};
             }
+
+            wordUsed.put(words[i], true);
         }
-        return answer;
+
+        return new int[]{0, 0};
     }
 }
